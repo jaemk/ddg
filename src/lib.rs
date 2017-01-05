36f133f693ddff588403263b8eff2bd06b5d788c
Aaron Power <theaaronepower@gmail.com>
2017-01-05 06:20:27 +0000

    forgot to pub the modules

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! This library provides a strongly typed wrapper around the DuckDuckGo Instant
 //! Answers API. Most of the documentation comes from the
 //! [DuckDuckGo Instant Answers API Documentation](https://duckduckgo.com/api)
-//! This library comes with reqwest by default for convience, however it can be
+//! This library comes with reqwest by default for convenience, however it can be
 //! disabled. If disabled the library will fallback to hyper for `IntoUrl` so it
 //! can be used with your own hyper client implementation.
 //!
@@ -27,8 +27,8 @@
 extern crate serde;
 extern crate serde_json;
 
-mod query;
-mod ddg_response {
+pub mod query;
+pub mod ddg_response {
     include!(concat!(env!("OUT_DIR"), "/serde_types.rs"));
 }
 
